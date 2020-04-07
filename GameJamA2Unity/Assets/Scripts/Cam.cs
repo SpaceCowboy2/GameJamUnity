34813cd05b26a71e6b34c21e57dbcc3719aa9cf8
loickGombauld <wild1907@hotmail.fr>
2020-04-07 10:03:47 +0200

    les trois C

---
+++ GameJamA2Unity/Assets/Scripts/Cam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    public GameObject Pointer;
+    public GameObject Player;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        Pointer.transform.eulerAngles = new Vector3(pitch, yaw);
+        Player.transform.eulerAngles = new Vector3(0, yaw);
+
+        if (pitch >= 90)
+        {
+            Pointer.transform.eulerAngles = new Vector3(90, yaw, 0.0f);
+            Player.transform.eulerAngles = new Vector3(0, yaw);
+        }
+        else if (pitch <= -90)
+        {
+            Pointer.transform.eulerAngles = new Vector3(-90, yaw, 0.0f);
+            Player.transform.eulerAngles = new Vector3(0, yaw);
+        }
+
+    }
+}
---
+++ GameJamA2Unity/Assets/Scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float vie;
+    public float movementSpeed;
+    public Rigidbody PlayerBody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.RightShift))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+            
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
