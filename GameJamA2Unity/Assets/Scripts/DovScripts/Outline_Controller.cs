ca70b6c104434cbef4e4c569d037fbbe79c3a279
Benzakk <dovbenzak2@hotmail.com>
2020-04-07 15:55:30 +0200

    Creation outline object

---
+++ GameJamA2Unity/Assets/Scripts/DovScripts/Outline_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline_Controller : MonoBehaviour
+{
+    private MeshRenderer renderer;
+    public float maxOutLineWidth;
+    public Color OutlineColor;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    public void ShowOutline()
+    {
+
+        foreach (var mat in renderer.materials)
+        { 
+            mat.SetFloat("_Outline", maxOutLineWidth);
+            mat.SetColor("_OutlineColor", OutlineColor);
+        }
+
+    }
+
+    public void HideOutline()
+    {
+        foreach (var mat in renderer.materials)
+        {
+            mat.SetFloat("_Outline", 0f);
+        }
+    }
+}
