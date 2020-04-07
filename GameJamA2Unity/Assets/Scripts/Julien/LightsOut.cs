032f5cd11fbdcf81a3864591e7919b32d25c8e2f
Julien Dor <julien.dor3@gmail.com>
2020-04-07 16:31:23 +0200

    Hallway with lights out

---
+++ GameJamA2Unity/Assets/Scripts/Julien/LightsOut.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOut : MonoBehaviour
+{
+    public List<GameObject> lights = new List<GameObject>();
+    private int listLength = 5;
+    private int _switch = 5;
+
+  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (listLength > 0 && listLength == _switch)
+        {
+            StartCoroutine(TurnOffLights());
+        }
+    }
+
+    IEnumerator TurnOffLights()
+    {
+        Debug.Log(listLength);
+        lights[listLength - 1].gameObject.SetActive(false);
+        listLength--;
+        yield return new WaitForSeconds(2);
+        _switch--;
+        
+        
+        
+    }
+}
