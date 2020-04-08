3fe03ddd654c62d77872a733d75f3e77c084ddb1
Julien Dor <julien.dor3@gmail.com>
2020-04-08 15:37:54 +0200

    Début ascenseur avec début animations

---
+++ GameJamA2Unity/Assets/Scripts/Julien/Gameplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    public int numberOfShakes = 5;
+    private Animator animShake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animShake = this.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Shaking());
+    }
+
+    IEnumerator Shaking()
+    {
+        if(numberOfShakes > 0)
+        {
+            animShake.Play("ShakeFreeFall");
+            numberOfShakes--;
+            Debug.Log(numberOfShakes);
+            yield return new WaitForSeconds(0.16f);
+        }
+        else
+        {
+            animShake.Play();
+        }
+    }
+}
