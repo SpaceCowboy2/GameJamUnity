cc293ac31c28d290c8e0fd629d131e047b24640b
Amir1632 <amir.alotounou@gmail.com>
2020-04-10 00:29:11 +0200

    LD : spotlight bouttons

--- GameJamA2Unity/Assets/Scripts/Julien/LightsOut.cs
+++ GameJamA2Unity/Assets/Scripts/Julien/LightsOut.cs
@@ -7,14 +7,15 @@ public class LightsOut : MonoBehaviour
     public List<GameObject> lights = new List<GameObject>();
     private int listLength = 5;
     private int _switch = 5;
+    public Light _light;
+    private Color32 red;
 
-  
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        red = new Color32(130, 18, 18, 255);
     }
 
     // Update is called once per frame
@@ -31,10 +32,15 @@ public class LightsOut : MonoBehaviour
         //Debug.Log(listLength);
         lights[listLength - 1].gameObject.SetActive(false);
         listLength--;
-        yield return new WaitForSeconds(25);
+        yield return new WaitForSeconds(5);
         _switch--;
-        
-        
-        
+
+        if (listLength == 1)
+        {
+            _light.color = red;
+        }
+
+
+
     }
 }
