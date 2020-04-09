8e46fa485e7f18f057da45a1fa1507e17e1722c3
loickGombauld <wild1907@hotmail.fr>
2020-04-09 13:04:51 +0200

    Mise à jour des hallucinations

--- GameJamA2Unity/Assets/Scripts/UI_Manager.cs
+++ GameJamA2Unity/Assets/Scripts/UI_Manager.cs
@@ -31,29 +31,12 @@ public void Damage()
         {
             Hallucination_F = 0;
         }
-        if (Hallucination_F>=10 && Hallucination_F<= 30)
-        {
-            var Cam = gameObject.GetComponent<PostProcessVolume >();
-            Cam.weight = 0.1f;
-            
-        }
-        else if (Hallucination_F >= 31 && Hallucination_F <= 50)
-        {
-            var Cam = gameObject.GetComponent<PostProcessVolume>();
-            var cam = PP.GetComponent<Volume>();
-            var Com = gameObject.GetComponent<Camera>();
-            Cam.weight = 0.3f;
-            cam.weight = 0.2f;
-            Com.fieldOfView = 57;
-        }
-        else if (Hallucination_F >= 51 && Hallucination_F <= 70)
-        {
-            var Cam = gameObject.GetComponent<PostProcessVolume>();
-            var cam = PP.GetComponent<Volume>();
-            var Com = gameObject.GetComponent<Camera>();
-            Cam.weight = 0.7f;
-            cam.weight = 0.5f;
-            Com.fieldOfView = 51;
-        }
+        var Com = gameObject.GetComponent<Camera>();
+        var Cam = gameObject.GetComponent<PostProcessVolume>();
+        var cam = PP.GetComponent<Volume>();
+        Com.fieldOfView = Hallucination_F/((Com.fieldOfView * 100) / 60);
+            Cam.weight = Hallucination_F/200;
+            cam.weight = Hallucination_F / 200;
+
     }
 }
