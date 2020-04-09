2492dd529548402e364ff85b9154540a54256489
Benzakk <dovbenzak2@hotmail.com>
2020-04-09 20:21:25 +0200

    Fin prog de l'enigme

---
+++ GameJamA2Unity/Assets/Scripts/DovScripts/Symbols.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Symbols : MonoBehaviour
+{
+    public string symbol;
+    public bool isValid = false;
+    public Color colorValid = Color.green;
+    public Color colorInValid = Color.red;
+
+    void Update()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name == symbol)
+            {
+                child.gameObject.SetActive(true);
+                if (isValid)
+                {
+                    child.GetComponent<Image>().color = colorValid;
+                }
+                else
+                {
+                    child.GetComponent<Image>().color = colorInValid;
+                }
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+}
